c767515bc6abc219a67341163cd27f8bc36800d3
Jacob Hafström <jacobhafstrom25@MacBook-Air-som-tillhor-Jacob.local>
2025-12-06 17:06:27 +0100

    Replace broken logos with Clearbit Logo API

--- src/data/akassor.ts
+++ src/data/akassor.ts
@@ -5,167 +5,165 @@ export type Akassa = {
     url: string;
 };
 
-export const akassor: Akassa[] = [
-    {
-        name: "Akademikernas a-kassa (AEA)",
+{
+    name: "Akademikernas a-kassa (AEA)",
         slug: "aea",
-        logo: "https://www.aea.se/wp-content/uploads/2021/06/aea-logo.svg",
-        url: "https://www.aea.se"
-    },
-    {
-        name: "Alfa-kassan",
+            logo: "https://logo.clearbit.com/aea.se",
+                url: "https://www.aea.se"
+},
+{
+    name: "Alfa-kassan",
         slug: "alfa",
-        logo: "https://www.alfakassan.se/wp-content/themes/alfakassan/assets/images/logo.svg",
-        url: "https://www.alfakassan.se"
-    },
-    {
-        name: "Seko a-kassa",
+            logo: "https://logo.clearbit.com/alfakassan.se",
+                url: "https://www.alfakassan.se"
+},
+{
+    name: "Seko a-kassa",
         slug: "seko",
-        logo: "https://www.sekosakassa.se/wp-content/themes/seko/img/logo.svg",
-        url: "https://www.sekosakassa.se"
-    },
-    {
-        name: "Vision a-kassa",
+            logo: "https://logo.clearbit.com/sekosakassa.se",
+                url: "https://www.sekosakassa.se"
+},
+{
+    name: "Vision a-kassa",
         slug: "vision",
-        logo: "https://www.akassanvision.se/wp-content/themes/vision/dist/images/logo.svg",
-        url: "https://www.akassanvision.se"
-    },
-    {
-        name: "Byggnads a-kassa",
+            logo: "https://logo.clearbit.com/akassanvision.se",
+                url: "https://www.akassanvision.se"
+},
+{
+    name: "Byggnads a-kassa",
         slug: "byggnads",
-        logo: "https://www.byggnads.se/globalassets/logos/byggnads_logo.svg",
-        url: "https://www.byggnadsakassa.se"
-    },
-    {
-        name: "Elektrikernas a-kassa",
+            logo: "https://logo.clearbit.com/byggnadsakassa.se",
+                url: "https://www.byggnadsakassa.se"
+},
+{
+    name: "Elektrikernas a-kassa",
         slug: "eak",
-        logo: "https://www.elektriker.se/globalassets/forbundet/logotyper/elektrikerna-logo.svg",
-        url: "https://www.elektrikernasakassa.se"
-    },
-    {
-        name: "Fastighets a-kassa",
+            logo: "https://logo.clearbit.com/elektrikernasakassa.se",
+                url: "https://www.elektrikernasakassa.se"
+},
+{
+    name: "Fastighets a-kassa",
         slug: "fastighets",
-        logo: "https://www.fastighets.se/globalassets/logos/fastighets_logo.svg",
-        url: "https://www.fastighetsakassa.se"
-    },
-    {
-        name: "Finans- och försäkringsbranschens a-kassa (FFAK)",
+            logo: "https://logo.clearbit.com/fastighetsakassa.se",
+                url: "https://www.fastighetsakassa.se"
+},
+{
+    name: "Finans- och försäkringsbranschens a-kassa (FFAK)",
         slug: "ffak",
-        logo: "https://www.ffakassan.se/wp-content/uploads/2020/09/ffak-logga.svg",
-        url: "https://www.ffakassan.se"
-    },
-    {
-        name: "GS a-kassa",
+            logo: "https://logo.clearbit.com/ffakassan.se",
+                url: "https://www.ffakassan.se"
+},
+{
+    name: "GS a-kassa",
         slug: "gs",
-        logo: "https://www.gsfacket.se/globalassets/gs/logo/gs-logo.svg",
-        url: "https://www.gsakassa.se"
-    },
-    {
-        name: "Hamnarbetarnas a-kassa",
+            logo: "https://logo.clearbit.com/gsakassa.se",
+                url: "https://www.gsakassa.se"
+},
+{
+    name: "Hamnarbetarnas a-kassa",
         slug: "hamn",
-        logo: "https://hamnarbetarna.se/wp-content/uploads/2020/11/hamnarbetare-logo.svg",
-        url: "https://hamnakassa.se"
-    },
-    {
-        name: "Handels a-kassa",
+            logo: "https://logo.clearbit.com/hamnakassa.se",
+                url: "https://hamnakassa.se"
+},
+{
+    name: "Handels a-kassa",
         slug: "handels",
-        logo: "https://www.handels.se/globalassets/logos/handels-logo.svg",
-        url: "https://www.handels.se/akassan"
-    },
-    {
-        name: "HRAK – Hotell & Restaurang",
+            logo: "https://logo.clearbit.com/handels.se",
+                url: "https://www.handels.se/akassan"
+},
+{
+    name: "HRAK – Hotell & Restaurang",
         slug: "hrak",
-        logo: "https://www.hrak.se/wp-content/themes/hrak/assets/img/logo.svg",
-        url: "https://www.hrak.se"
-    },
-    {
-        name: "IF Metalls a-kassa",
+            logo: "https://logo.clearbit.com/hrak.se",
+                url: "https://www.hrak.se"
+},
+{
+    name: "IF Metalls a-kassa",
         slug: "ifmetall",
-        logo: "https://www.ifmetall.se/globalassets/logotyper/if-metall-logotyp.svg",
-        url: "https://www.ifmetall.se/akassan"
-    },
-    {
-        name: "Journalisternas a-kassa",
+            logo: "https://logo.clearbit.com/ifmetall.se",
+                url: "https://www.ifmetall.se/akassan"
+},
+{
+    name: "Journalisternas a-kassa",
         slug: "journalist",
-        logo: "https://www.sjf.se/themes/custom/sjf/logo.svg",
-        url: "https://www.journalisternas-akassa.se"
-    },
-    {
-        name: "Kommunals a-kassa",
+            logo: "https://logo.clearbit.com/journalisternas-akassa.se",
+                url: "https://www.journalisternas-akassa.se"
+},
+{
+    name: "Kommunals a-kassa",
         slug: "kommunal",
-        logo: "https://www.kommunal.se/themes/custom/kommunal/logo.svg",
-        url: "https://www.kommunalsakassa.se"
-    },
-    {
-        name: "Ledarnas a-kassa",
+            logo: "https://logo.clearbit.com/kommunalsakassa.se",
+                url: "https://www.kommunalsakassa.se"
+},
+{
+    name: "Ledarnas a-kassa",
         slug: "ledarna",
-        logo: "https://www.ledarna.se/globalassets/logos/ledarna_logo.svg",
-        url: "https://www.ledarnasakassa.se"
-    },
-    {
-        name: "Livs a-kassa",
+            logo: "https://logo.clearbit.com/ledarnasakassa.se",
+                url: "https://www.ledarnasakassa.se"
+},
+{
+    name: "Livs a-kassa",
         slug: "livs",
-        logo: "https://www.livs.se/wp-content/themes/livs/assets/img/logo-livs.svg",
-        url: "https://www.livs-akassa.se"
-    },
-    {
-        name: "Lärarnas a-kassa",
+            logo: "https://logo.clearbit.com/livs-akassa.se",
+                url: "https://www.livs-akassa.se"
+},
+{
+    name: "Lärarnas a-kassa",
         slug: "la",
-        logo: "https://www.lararnasakassa.nu/wp-content/themes/akassan/assets/img/logo.svg",
-        url: "https://www.lararnasakassa.nu"
-    },
-    {
-        name: "Pappers a-kassa",
+            logo: "https://logo.clearbit.com/lararnasakassa.nu",
+                url: "https://www.lararnasakassa.nu"
+},
+{
+    name: "Pappers a-kassa",
         slug: "pappers",
-        logo: "https://www.pappers.se/sites/default/files/pappers_logotyp.svg",
-        url: "https://www.pappersakassa.se"
-    },
-    {
-        name: "SLAK – Skogs- och lantbrukstjänstemän",
+            logo: "https://logo.clearbit.com/pappersakassa.se",
+                url: "https://www.pappersakassa.se"
+},
+{
+    name: "SLAK – Skogs- och lantbrukstjänstemän",
         slug: "slak",
-        logo: "https://slf-facket.se/wp-content/uploads/2023/08/slf-logo.svg",
-        url: "https://slf-facket.se"
-    },
-    {
-        name: "Småföretagarnas a-kassa (SmåA)",
+            logo: "https://logo.clearbit.com/slf-facket.se",
+                url: "https://slf-facket.se"
+},
+{
+    name: "Småföretagarnas a-kassa (SmåA)",
         slug: "smaa",
-        logo: "https://www.smakassa.se/wp-content/uploads/2018/08/sma-logotyp.svg",
-        url: "https://www.smakassa.se"
-    },
-    {
-        name: "STs a-kassa",
+            logo: "https://logo.clearbit.com/smakassa.se",
+                url: "https://www.smakassa.se"
+},
+{
+    name: "STs a-kassa",
         slug: "sts",
-        logo: "https://www.st.org/themes/custom/st/logo.svg",
-        url: "https://www.stsakassa.se"
-    },
-    {
-        name: "Svensk Handels a-kassa",
+            logo: "https://logo.clearbit.com/stsakassa.se",
+                url: "https://www.stsakassa.se"
+},
+{
+    name: "Svensk Handels a-kassa",
         slug: "sha",
-        logo: "https://www.akassan.com/wp-content/uploads/2020/04/sha-logo.svg",
-        url: "https://www.akassan.com"
-    },
-    {
-        name: "Sveriges Arbetares a-kassa (SAAK)",
+            logo: "https://logo.clearbit.com/akassan.com",
+                url: "https://www.akassan.com"
+},
+{
+    name: "Sveriges Arbetares a-kassa (SAAK)",
         slug: "saak",
-        logo: "https://www.saak.se/wp-content/themes/saak/img/logo.svg",
-        url: "https://www.saak.se"
-    },
-    {
-        name: "Säljarnas a-kassa",
+            logo: "https://logo.clearbit.com/saak.se",
+                url: "https://www.saak.se"
+},
+{
+    name: "Säljarnas a-kassa",
         slug: "saljarnas",
-        logo: "https://www.saljarnas.se/wp-content/themes/saljarnas/img/logo.svg",
-        url: "https://www.saljarnas-akassa.se"
-    },
-    {
-        name: "Transports a-kassa",
+            logo: "https://logo.clearbit.com/saljarnas-akassa.se",
+                url: "https://www.saljarnas-akassa.se"
+},
+{
+    name: "Transports a-kassa",
         slug: "transport",
-        logo: "https://www.transport.se/globalassets/forbundet/logotyp/logo-transport.svg",
-        url: "https://www.transportsakassa.se"
-    },
-    {
-        name: "Unionens a-kassa",
+            logo: "https://logo.clearbit.com/transportsakassa.se",
+                url: "https://www.transportsakassa.se"
+},
+{
+    name: "Unionens a-kassa",
         slug: "unionen",
-        logo: "https://www.unionen.se/themes/custom/unionen/logo.svg",
-        url: "https://www.unionensakassa.se"
-    }
-];
+            logo: "https://logo.clearbit.com/unionensakassa.se",
+                url: "https://www.unionensakassa.se"
+}
