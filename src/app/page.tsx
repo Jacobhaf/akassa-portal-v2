4490ecd1521d06ccc08fc16b2939316b0d48397c
Jacob Hafström <jacobhafstrom25@MacBook-Air-som-tillhor-Jacob.local>
2025-12-07 19:56:31 +0100

    chore: update Jämför nu button link to /jamfor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Home() {
                         </div>
                         <div className="mt-10 flex items-center gap-x-6">
                             <Link
-                                href="#jamfor"
+                                href="/jamfor"
                                 className="rounded-md bg-green-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500"
                             >
                                 Jämför nu
